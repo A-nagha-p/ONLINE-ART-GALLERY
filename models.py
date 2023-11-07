c21f110608b50be2954cd371d46e45a1a79c017c
Anagha P <117934252+A-nagha-p@users.noreply.github.com>
2023-11-07 04:08:09 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -6,3 +6,32 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    theme = models.CharField(max_length=255, blank=True, null=True)
+    art_type = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=[('available', 'Available'), ('out-of-stock', 'Out of Stock')])
+    product_image = models.ImageField(upload_to='product_images/', max_length=255)
+
+    def __str__(self):
+        return self.product_name
+
+
+
+class Competition(models.Model):
+    competition_name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='competition_images/', max_length=255)
+    am_pm = models.CharField(max_length=2, choices=[("AM", "AM"), ("PM", "PM")])
+
+    def __str__(self):
+        return self.competition_name
+
+
--- views.py
+++ views.py
@@ -1,26 +1,73 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render ,redirect,get_object_or_404
 from django.contrib.auth import authenticate, login as auth_login
-from .models import CustomUser  # Assuming you have a CustomUser model
+from .models import CustomUser,Competition  # Assuming you have a CustomUser model
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+from django.contrib import auth
+from django.contrib import messages
+from django.contrib.auth.models import User
+from store.models import CustomUser
+from django.http import HttpResponse
+from .models import Product
 
+
+@never_cache
 def index(request):
     return render(request,'index.html')
-
+@never_cache
 def blog(request):
     return render(request,'blog.html')
+@never_cache
 def about(request):
     return render(request,'about.html')
+@never_cache
 def contact(request):
     return render(request,'contact.html')
+@never_cache
 def exhibition(request):
     return render(request,'exhibition.html')
+@never_cache
 def competition(request):
     return render(request,'competition.html')
+@never_cache
 def artstore(request):
     return render(request,'artstore.html')
+#@never_cache
+#@login_required(login_url='login')
+#def admindash(request):
+    #return render(request,'admindash.html')
 
 
+@never_cache
+def compapply(request):
+    return render(request,'compapply.html')
+@never_cache
+def blogone(request):
+    return render(request,'blogone.html')
 
+@never_cache
+@login_required(login_url='login')
+def userprofile(request):
+    if 'username' in request.session:
+        response = render(request,'userprofile.html')
+        response['Cache-Control'] = 'no-store,must-revalidate'
+        return response
+    else:
+        return redirect('login')
+
+@never_cache
+@login_required(login_url='login')
+def artistprofile(request):
+    if 'username' in request.session:
+        response = render(request,'artistprofile.html')
+        response['Cache-Control'] = 'no-store,must-revalidate'
+        return response
+    else:
+        return redirect('login')
+
+
+@never_cache
 def register(request):
      if request.method == 'POST':
         full_name = request.POST['full_name']
@@ -47,6 +94,7 @@ def register(request):
 from django.contrib.auth import authenticate, login as auth_login
 from django.shortcuts import render, redirect
 
+@never_cache
 def login(request):
     if request.method == 'POST':
         loginusername = request.POST.get('username')
@@ -57,7 +105,15 @@ def login(request):
 
             if user is not None:
                 auth_login(request, user)
-                return redirect('index')
+                request.session['username'] = user.username
+                if user.user_type == 'user':
+                    messages.success(request, 'Login successful')
+                    return redirect('userprofile')
+                elif user.user_type == 'artist':
+                    messages.success(request, 'Login successful')
+                    return redirect('artistprofile')
+                else:
+                    return redirect('admindash')
             else:
                 error_message = 'Invalid username or password'
                 return render(request, 'login.html', {'error_message': error_message})
@@ -65,5 +121,196 @@ def login(request):
             error_message = 'Username and password are required'
             return render(request, 'login.html', {'error_message': error_message})
 
-    return render(request, 'login.html')
+    response = render(request,'login.html')
+    response['Cache-Control'] = 'no-store,must-revalidate'
+    return response
+
+@never_cache
+def handlelogout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('login')
+
+
+@never_cache
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
+
+
+@never_cache
+@login_required(login_url='login')
+def admindash(request):
+    if request.user.is_superuser:
+        users = CustomUser.objects.exclude(is_superuser=True)
+        return render(request, "admindash.html", {"users": users})
+    return redirect("home")
+
+@never_cache
+def addproduct(request):
+    return render(request,'addproduct.html')
+
+@never_cache
+def blogupload(request):
+    return render(request,'blogupload.html')
+
+@never_cache
+def editprofile(request):
+    # Your edit profile logic here
+    return render(request, 'editprofile.html')  # Replace with your template and logic
+
+@never_cache
+@login_required(login_url='login')
+def regusers(request):
+    if request.user.is_superuser:
+        # Fetch users with user_type 'user' and exclude superusers
+        users = CustomUser.objects.filter(user_type='user', is_superuser=False)
+
+        return render(request, 'regusers.html', {'users': users})
+    else:
+        return redirect('home')
+
+
+@never_cache
+@login_required(login_url='login')
+def regartist(request):
+    if request.user.is_superuser:
+        # Fetch users with user_type 'user' and exclude superusers
+        users = CustomUser.objects.filter(user_type='artist', is_superuser=False)
+
+        return render(request, 'regartist.html', {'users': users})
+    else:
+        return redirect('home')
+
+@never_cache
+def add_product(request):
+    if request.method == 'POST':
+        # Extract form data from the request
+        product_name = request.POST['productName']
+        theme = request.POST['theme']
+        art_type = request.POST['artType']
+        quantity = request.POST['quantity']
+        description = request.POST['description']
+        price = request.POST['price']
+        status = request.POST['status']
+        
+        # Assuming you have a file input with name "productImage"
+        product_image = request.FILES['productImage']
+
+        # Create a new Product object and save it to the database
+        product = Product(
+            product_name=product_name,
+            theme=theme,
+            art_type=art_type,
+            quantity=quantity,
+            description=description,
+            price=price,
+            status=status,
+            product_image=product_image
+        )
+        product.save()
+
+        # Redirect to a success page or do something else
+        return redirect('addproduct')
+
+    # If the request method is not POST, render the form page
+    return render(request, 'addproduct.html')
+
+
+
+@never_cache
+def viewproduct(request):
+    # Query your products from the database
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'viewproduct.html', context)
+
+
+@never_cache
+def base(request):
+    return render(request,'base.html')
+
+
+
+@never_cache  
+def product_list(request):
+    # Query your products from the database
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'exhibition.html', context)
+
+@never_cache
+def product_details(request, id):
+    # Retrieve the product details from the database
+    product = get_object_or_404(Product, id=id)
+
+    # Render the product details template with the product data
+    return render(request, 'productdetails.html', {'product': product})
+
+
+
+
+@never_cache
+@login_required(login_url='login')
+def add_competition(request):
+    if request.method == 'POST':
+        competition_name = request.POST['competition_name']
+        description = request.POST['description']
+        date = request.POST['date']
+        time = request.POST['time']
+        am_pm = request.POST['ampm']
+        location = request.POST['location']
+        image = request.FILES['productImage']  # Change 'image' to 'productImage'
+
+        competition = Competition(
+            competition_name=competition_name,
+            description=description,
+            date=date,
+            time=time,
+            am_pm=am_pm,
+            location=location,
+            image=image
+        )
+        competition.save()
+
+        return redirect('competition')
+
+    return render(request, 'addcompetition.html')
+
+@never_cache
+def competition(request):
+    # Fetch a list of competitions from the database
+    competitions = Competition.objects.all()
+    
+    return render(request, 'competition.html', {'competitions': competitions})
+
+
+@never_cache
+def viewcompetition(request):
+    return render(request,'viewcompetition.html')
+
+
+@never_cache
+def viewcompetition(request):
+    # Query competitions from the database
+    competitions = Competition.objects.all()
+
+    context = {
+        'competitions': competitions,
+    }
+
+    return render(request, 'viewcompetition.html', context)
+
+
+
+
+
 
